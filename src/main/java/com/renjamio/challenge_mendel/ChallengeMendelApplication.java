33445d411c1b7caca990571be35e369aca63e5dd
rodrigo <renjamio@gmail.com>
2022-05-20 10:51:18 -0300

    first commit, dependencies

---
+++ src/main/java/com/renjamio/challenge_mendel/ChallengeMendelApplication.java
@@ -0,0 +1,13 @@
+package com.renjamio.challenge_mendel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeMendelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeMendelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renjamio/challenge_mendel/ChallengeMendelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renjamio.challenge_mendel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeMendelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
